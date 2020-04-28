f2c48b3eb14c7ac4539cb30e9e39963238c54090
Atsushi Kurata <3.s.river@gmail.com>
2020-04-28 17:05:26 +0900

    New Project

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,9 +1,18 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+
+    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
