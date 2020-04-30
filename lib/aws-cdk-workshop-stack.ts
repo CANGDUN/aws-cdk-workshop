d7fd8fd8cced379bc9b3aa8e0067a7e5e6d10795
Atsushi Kurata <3.s.river@gmail.com>
2020-05-01 02:19:15 +0900

    Hello Lambda / update package.json

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,18 +1,15 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X,    // exection environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" direcrory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
