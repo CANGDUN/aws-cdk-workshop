3cc4bc930183020d9e036750547956066216975f
Atsushi Kurata <3.s.river@gmail.com>
2020-05-02 18:20:06 +0900

    API Gateway / Change Indent Size

--- lib/aws-cdk-workshop-stack.ts
+++ lib/aws-cdk-workshop-stack.ts
@@ -1,15 +1,21 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
 
 export class AwsCdkWorkshopStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // defines an AWS Lambda resource
-    const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X,    // exection environment
-      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" direcrory
-      handler: 'hello.handler'                // file is "hello", function is "handler"
-    });
-  }
+        // defines an AWS Lambda resource
+        const hello = new lambda.Function(this, 'HelloHandler', {
+            runtime: lambda.Runtime.NODEJS_10_X,    // exection environment
+            code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" direcrory
+            handler: 'hello.handler'                // file is "hello", function is "handler"
+        });
+
+        // defines an API Geteway REST API resource backed by our "hello" function.
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: hello
+        });
+    }
 }
